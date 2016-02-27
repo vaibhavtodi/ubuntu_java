a469ede48001000ac1503dbfcf087fc46b0d74a5
Vaibhav Todi <vaibhavtodi1989@gmail.com>
2016-02-27 10:35:55 +0530

    Updating the entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,7 @@ exec 2>&1
 set -e
 
 # Exporting the java path
+export  JAVA=/usr/lib/jvm/java-8-oracle
 
-
-# Setting up ElasticSearch
-sed    -i     's/es_cluster/$ES_CLUSTER_NAME/'          /etc/elasticsearch/elasticsearch.yml
-sed    -i     's/es_data_path/$ES_DATA_PATH/'           /etc/elasticsearch/elasticsearch.yml
-sed    -i     's/es_log_path/$ES_LOG_PATH/'             /etc/elasticsearch/elasticsearch.yml
-sed    -i     's/es_network_host/$ES_NETWORK_HOST/'     /etc/elasticsearch/elasticsearch.yml
-sed    -i     's/es_http_port/$ES_HTTP_PORT/'           /etc/elasticsearch/elasticsearch.yml
-sed    -i     's/es_unicast/$ES_UNICAST/'               /etc/elasticsearch/elasticsearch.yml
-
-# Starting the elasticsearch
-exec   
+#  Starting the java process
+exec    java
